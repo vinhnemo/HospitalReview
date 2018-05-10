eec2f011eebcf0a02ad2d8edc7ded57b75fb9c60
TG <tien.grimes@gmail.com>
2018-05-10 20:21:46 +0700

    Patient

---
+++ src/main/java/DAO/PatientDAO.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Database.*;
+import User.Patient;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+
+/**
+ *
+ * @author TGMaster
+ */
+public class PatientDAO {
+        
+    // Check existing username
+    public boolean validateUser(String username) {
+
+        String query = "SELECT * FROM users WHERE user_name = ?";
+
+        // Connect to database
+        Connection connection = Database.getConnection();
+
+        try {
+            PreparedStatement ps = connection.prepareCall(query);
+            ps.setString(1, username);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                connection.close();
+                return true;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    // Insert account
+    public boolean insertUser(Patient patient) {
+
+        String query = "INSERT INTO users (user_name, user_pass, user_email, user_dob, user_address, user_image, user_admin) VALUES "
+                + "(?, ?, ?, STR_TO_DATE(?,'%d-%m-%Y'), ?, ?, ?)";
+
+        // Connect to database
+        Connection connection = Database.getConnection();
+
+        try {
+            PreparedStatement ps = connection.prepareCall(query);
+            ps.setString(1, patient.getName());
+            ps.setString(2, patient.getPass());
+            ps.setString(3, patient.getEmail());
+            ps.setString(4, patient.getDOB());
+            ps.setString(5, patient.getAddress());
+            ps.setString(6, patient.getImage());
+            ps.setBoolean(7, patient.isAdmin());
+            ps.executeUpdate();
+
+            connection.close();
+            return true;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return false;
+    }
+
+    // Check login
+    public Patient login(String name) {
+        Patient p = new Patient();
+
+        String query = "SELECT * FROM users WHERE user_name = ?";
+
+        // Connect to database
+        Connection connection = Database.getConnection();
+
+        try {
+            PreparedStatement ps = connection.prepareCall(query);
+            ps.setString(1, name);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                p.setID(rs.getInt("user_id"));
+                p.setEmail(rs.getString("user_email"));
+                p.setName(rs.getString("user_name"));
+                p.setPass(rs.getString("user_pass"));
+                p.setAddress(rs.getString("user_address"));
+                p.setImage(rs.getString("user_image"));
+
+                // Format Date
+                if (rs.getDate("user_DOB") != null) {
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                    java.util.Date bday = rs.getDate("user_DOB");
+                    p.setDOB(sdf.format(bday));
+                }
+                psetAdmin(rs.getBoolean("user_admin"));
+            }
+
+            connection.close();
+            return p;
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public void updateUser(Patient p) {
+        String query = "UPDATE users SET user_name=?, user_pass=?, user_email=?, user_dob=STR_TO_DATE(?,'%d/%m/%Y'), user_address=?, user_image=? WHERE user_name=?";
+
+        // Connect to database
+        Connection connection = Database.getConnection();
+
+        try {
+            PreparedStatement ps = connection.prepareCall(query);
+            ps.setString(1, p.getName());
+            ps.setString(2, p.getPass());
+            ps.setString(3, p.getEmail());
+            ps.setString(4, p.getDOB());
+            ps.setString(5, p.getAddress());
+            ps.setString(6, p.getImage());
+            ps.setString(7, p.getName());
+            ps.executeUpdate();
+
+            connection.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/User/Doctor.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package User;
+
+/**
+ *
+ * @author TGMaster
+ */
+public class Doctor {
+    private int ID;
+    private String fname;
+    private String lname;
+    private String sex;
+    private String degree;
+    private String insurance;
+    private String speciality;
+    private String hours;
+    private String lang;
+
+    public Doctor() {
+    }
+
+    public Doctor(int ID, String fname, String lname, String sex, String degree, String insurance, String speciality, String hours, String lang) {
+        this.ID = ID;
+        this.fname = fname;
+        this.lname = lname;
+        this.sex = sex;
+        this.degree = degree;
+        this.insurance = insurance;
+        this.speciality = speciality;
+        this.hours = hours;
+        this.lang = lang;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getInsurance() {
+        return insurance;
+    }
+
+    public void setInsurance(String insurance) {
+        this.insurance = insurance;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public String getHours() {
+        return hours;
+    }
+
+    public void setHours(String hours) {
+        this.hours = hours;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+    
+    
+}
