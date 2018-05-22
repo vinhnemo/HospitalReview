68b095153695748cbaab06141b1ac46af3fa2a4b
TG <tien.grimes@gmail.com>
2018-05-22 09:14:05 +0700

    new UI new life

--- src/main/java/User/Login.java
+++ src/main/java/User/Login.java
@@ -44,6 +44,63 @@ public class Login extends HttpServlet {
         if (action == null) {
             rd = sc.getRequestDispatcher("/login.jsp");
             rd.forward(request, response);
+        } else if (action.equals("Login")) {
+            String email = request.getParameter("email");
+            String pass = request.getParameter("password");
+            String remember = request.getParameter("remember");
+
+            if (email.equals("") || pass.equals("")) {
+                error += "Please type email and password.";
+            } else {
+                patient = patientDAO.login(email);
+
+                if (patient.getPass() == null) {
+                    error += "User does not exist";
+                    adminLogin = true;
+                } else if (!PasswordHashing.checkPassword(pass, patient.getPass())) {
+                    error += "Please enter valid username or password";
+                    adminLogin = true;
+                }
+
+                if (adminLogin) {
+                    admin = adminDAO.login(email);
+
+                    // Check if user does not exist
+                    if (admin.getPass() == null) {
+                        error += "User does not exist";
+                    } else if (!PasswordHashing.checkPassword(pass, admin.getPass())) {
+                        error += "Please enter valid username or password";
+                    }
+                }
+            }
+
+            // If something is wrong
+            if (error.length() > 0) {
+                response.getWriter().write(error);
+            } else {
+                // If this is an admin
+                if (adminLogin) {
+                    session.setAttribute("user", admin);
+                } else {
+                    session.setAttribute("user", patient);
+                }
+
+                // Save login cookie
+                if (remember != null) {
+                    Cookie loginCookie;
+                    if (adminLogin) {
+                        loginCookie = new Cookie("u_email", admin.getEmail());
+                    } else {
+                        loginCookie = new Cookie("u_email", patient.getEmail());
+                    }
+                    //setting cookie to expiry in 30 mins
+                    loginCookie.setMaxAge(60 * 60 * 24 * 365);
+                    response.addCookie(loginCookie);
+                }
+
+                response.sendRedirect("/home.jsp");
+            }
+
         } else {
 
             // Get input from jsp
@@ -58,7 +115,7 @@ public class Login extends HttpServlet {
 
                 // Check if user does not exist
                 if (patient.getPass() == null) {
-                    error += "User does not exist.";
+                    error += "User does not exist";
                     adminLogin = true;  // In case user is not patient
 
                     // If exist then check password
@@ -72,7 +129,7 @@ public class Login extends HttpServlet {
 
                     // Check if user does not exist
                     if (admin.getPass() == null) {
-                        error += "User does not exist.";
+                        error += "User does not exist";
                     } else if (!PasswordHashing.checkPassword(pass, admin.getPass())) {
                         error += "Please enter valid username or password";
                     }
@@ -107,7 +164,7 @@ public class Login extends HttpServlet {
                     response.addCookie(loginCookie);
                 }
 
-                response.sendRedirect("/profilePatient.jsp");
+                response.sendRedirect("/home.jsp");
             }
         }
 
