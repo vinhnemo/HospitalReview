f47a0c42d1d59fdcd838b50a0c365b06ae597602
TG <tien.grimes@gmail.com>
2018-05-24 21:29:55 +0700

    update ignore database

--- src/main/java/Database/Database.java
+++ src/main/java/Database/Database.java
@@ -9,7 +9,7 @@ public class Database {
         Connection conn = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://35.198.229.164:3306/", "root", "12345");
+            conn = DriverManager.getConnection("jdbc:mysql://35.198.229.164:3306/hospital", "root", "12345");
 
         } catch (ClassNotFoundException | SQLException e) {
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, e);
