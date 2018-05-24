db6e3c7c97b5ff57fbee2454d5b766e43f62cb67
duyetphamm <39182779+duyetphamm@users.noreply.github.com>
2018-05-24 07:07:17 -0700

    Update DoctorDAO.java

--- src/main/java/User/DAO/DoctorDAO.java
+++ src/main/java/User/DAO/DoctorDAO.java
@@ -7,7 +7,6 @@ package User.DAO;
 
 import Database.*;
 import User.DTO.Doctor;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -116,7 +115,7 @@ public class DoctorDAO {
     }
 
     public Doctor getDoctor(int id) {
-        String query = "SELECT * FROM doctor WHERE d_id = ?";
+        String query = "SELECT doctor.d_id,d_fname,d_lname,d_gender,d_degree,d_insurance,d_speciality,d_hour,languages, allowReview FROM doctor,doctorreview WHERE doctor.d_id = ? AND doctor.d_id = doctorreview.d_id;";
         Doctor doctor = new Doctor();
 
         // Connect to database
@@ -137,6 +136,7 @@ public class DoctorDAO {
                 doctor.setSpeciality(rs.getString("d_speciality"));
                 doctor.setHours(rs.getString("d_hour"));
                 doctor.setLang(rs.getString("languages"));
+                doctor.setAllowReview(rs.getInt("allowReview"));
             }
 
             connection.close();
@@ -145,9 +145,12 @@ public class DoctorDAO {
         }
         return doctor;
     }
-
+    
+    
     public void addDoctor(int id, String fname, String lname, String sex, String degree, boolean insurance, String speciality, String hours, String lang) {
 
+        ResultSet generatedKeys;
+        int ID = 0;
         String query = "INSERT INTO doctor"
                 + "("
                 + "d_id,"
@@ -171,26 +174,59 @@ public class DoctorDAO {
                 + "?,"
                 + "?);";
 
-        // Connect to database
-        Connection connection = Database.getConnection();
+        String query1 = "INSERT INTO doctorreview(d_id) VALUES (?)";
 
         try {
-            PreparedStatement ps = connection.prepareCall(query);
-            ps.setInt(1, id);
-            ps.setString(2, fname);
-            ps.setString(3, lname);
-            ps.setString(4, sex);
-            ps.setString(5, degree);
-            ps.setBoolean(6, insurance);
-            ps.setString(7, speciality);
-            ps.setString(8, hours);
-            ps.setString(9, lang);
-            ps.executeUpdate();
+            // create a mysql database connection
+            String myDriver = "com.mysql.jdbc.Driver";
+            String myUrl = "jdbc:mysql://localhost:3306/onlshopnew?useUnicode=true&characterEncoding=UTF-8&useFastDateParsing=false";
+            Class.forName(myDriver);
+            Connection conn = DriverManager.getConnection(myUrl, "root", "7777777");
 
-            connection.close();
+            conn.setAutoCommit(false);
+            // set all the preparedstatement parameters
+            PreparedStatement stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            stmt.setInt(1, id);
+            stmt.setString(2, fname);
+            stmt.setString(3, lname);
+            stmt.setString(4, sex);
+            stmt.setString(5, degree);
+            stmt.setBoolean(6, insurance);
+            stmt.setString(7, speciality);
+            stmt.setString(8, hours);
+            stmt.setString(9, lang);
+            stmt.executeUpdate();
 
-        } catch (SQLException ex) {
-            ex.printStackTrace();
+            conn.commit();
+            generatedKeys = stmt.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                ID = generatedKeys.getInt(1);
+            } else {
+                throw new SQLException("No generated user ID returned");
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println("Got an exception!");
+            System.err.println(e.getMessage());
+        }
+
+        try {
+            // create a mysql database connection
+            String myDriver = "com.mysql.jdbc.Driver";
+            String myUrl = "jdbc:mysql://localhost:3306/onlshopnew?useUnicode=true&characterEncoding=UTF-8&useFastDateParsing=false";
+            Class.forName(myDriver);
+            Connection conn = DriverManager.getConnection(myUrl, "root", "7777777");
+
+            // set all the preparedstatement parameters
+            PreparedStatement stmt = conn.prepareStatement(query1);
+            stmt.setInt(1, ID);
+
+            // execute the preparedstatement insert
+            stmt.executeUpdate();
+            stmt.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println("Got an exception!");
+            System.err.println(e.getMessage());
         }
 
     }
@@ -199,10 +235,20 @@ public class DoctorDAO {
         // Connect to database
         Connection connection = Database.getConnection();
 
-        String query = "DELETE FROM doctor WHERE d_id = ?";
+        String query = "DELETE FROM doctorreview WHERE d_id = ?";
+        String query1 = "DELETE FROM doctor WHERE d_id = ?";
 
         try {
             PreparedStatement ps = connection.prepareCall(query);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+            connection.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DoctorDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
+            PreparedStatement ps = connection.prepareCall(query1);
             ps.setInt(1, id);
             ps.executeUpdate();
             connection.close();
